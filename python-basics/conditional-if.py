86512df54dcf2df665188e24c7b4633ea018ad44
Bhagya256-web <bhagyak2728.com>
2026-03-09 20:07:58 +0530

    Added matplotlib folder

---
+++ python-basics/conditional-if.py
@@ -0,0 +1,19 @@
+name_str=input("Hi what is your name?")
+age=input("Hi "+name_str+", what is your age")
+ 
+password="123123123"
+
+if password=="secret@123":
+    print("Access granted")
+else:
+    print("access denied")
+
+    
+# if int(age)>=18:
+#     print("you are an adult")
+#     print("you can vote!")
+# else:
+#     print(" you are a child")
+#     print("go have some ice cream")
+
+
---
+++ python-basics/varibales.py
@@ -0,0 +1,32 @@
+#allowed
+myage=33
+my_age=33
+_my_age=33
+myAge=33
+MYAGE=33
+myage1=33
+
+#wrong/illegal
+#1myage=33
+#my-var=33
+#my var=33
+
+#assignments
+a,b,c="Bhagya","Rakshitha","Anjali"
+print(a)
+print(b)
+print(c)
+
+d=e=f="Bhagya"
+print(d)
+print(e)
+print(f)
+
+name="Anjali"
+age=33
+height=5.5
+is_married=True
+
+print("Hi I am ",name)
+print("Iam ",age, "years old")
+print("My height is ")
